0d57be3fe87a5acae96586df7c1950994951d9e8
Tristin-Young <86166175+Tristin-Young@users.noreply.github.com>
2023-05-12 23:57:05 -0400

    Added sorting by occurences as well as capped output to 20. Need to now sort input to only be the poem

--- src/module_2_assignment/CountWords.java
+++ src/module_2_assignment/CountWords.java
@@ -2,8 +2,12 @@ package module_2_assignment;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class CountWords {
@@ -29,9 +33,18 @@ public class CountWords {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 
-		Map<String, Integer> words = new HashMap<String, Integer>();
-		countEachWord("C:\\Users\\trist\\Desktop\\Software Dev 1\\Module-2-Assignment\\Module 2 Assignment Input.htm", words);
-		System.out.println(words + "\n");
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		countEachWord("C:\\Users\\trist\\Desktop\\Software Dev 1\\Module-2-Assignment\\Module 2 Assignment Input.htm", map);
+		
+		List <Entry<String, Integer>> nlist = new ArrayList<>(map.entrySet());
+		nlist.sort(Entry.comparingByValue(Comparator.reverseOrder()));
+		
+		for(int i = 0; i < 20; i++)
+		{
+			System.out.println(i+1 + ":" + nlist.get(i));
+		}
+		//nlist.forEach(System.out::println);
+		//Stream<Map.Entry<K, V>> sorted = map.entrySet().stream().sorted(Map.Entry.comparingByValue()).limit(20).forEach(System.out::println);
 	}
 
 }
